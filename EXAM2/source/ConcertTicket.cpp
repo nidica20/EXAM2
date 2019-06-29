eb1988b068ec2d7937ba0de2c085abd57a9f98b8
nidica20 <45465822+nidica20@users.noreply.github.com>
2019-06-28 22:59:20 -0700

    Add files via upload

--- EXAM2/source/ConcertTicket.cpp
+++ EXAM2/source/ConcertTicket.cpp
@@ -5,7 +5,6 @@
 * Implements the leave method
 */
 void ConcertTicket::leave() {
-	// FIXME 4: Implement leave for baseball games
 	ConcertEvent * actual_event = dynamic_cast<ConcertEvent *>(the_event); // down-cast base class to derived class
 	actual_event->remove(me); // call ConcertEvent remove method to erase event goer identified by "me"
 }
